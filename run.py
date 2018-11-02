88900007693152fe7bceafe0ebe9b0b23245f2f5
araalifarooq <farooqp35h@gmail.com>
2018-11-02 23:35:59 +0300

    Debugging

--- run.py
+++ run.py
@@ -15,8 +15,8 @@ def content_not_found(error):
 @app.errorhandler(500)
 def internal_server_error(error):
     return jsonify({'message': "Internal server error"}),500
+dbUtils = DBConnection()
 
 if __name__ == "__main__":
-    dbUtils = DBConnection()
     dbUtils.create_tables()
     app.run(debug=True)
